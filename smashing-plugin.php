c19ecaeac64af4aa397518c314b8e8e37e233137
Евгений Трубин <onlajn@bk.ru>
2020-07-28 19:14:02 +0300

    yandex screen ads

--- smashing-plugin.php
+++ smashing-plugin.php
@@ -2,17 +2,22 @@
 /*
 	Plugin Name: WP corrector
 	Description: WP corrector (git)
-	Version: 0.0.3
+	Version: 0.0.4
 	Author: Трубин Евгений
 */
-if( ! class_exists( 'Smashing_Updater' ) ){
-    include_once( plugin_dir_path( __FILE__ ) . 'updater.php' );
+if (!class_exists('Smashing_Updater')) {
+    include_once(plugin_dir_path(__FILE__) . 'updater.php');
 }
 
-$updater = new Smashing_Updater( __FILE__ );
-$updater->set_username( 'prospectroRRM' );
-$updater->set_repository( 'wp-corrector' );
+$updater = new Smashing_Updater(__FILE__);
+$updater->set_username('prospectroRRM');
+$updater->set_repository('wp-corrector');
 /*
 	$updater->authorize( 'abcdefghijk1234567890' ); // Your auth code goes here for private repos
 */
 $updater->initialize();
+
+
+add_action('wp_enqueue_scripts', function () {
+    wp_enqueue_style('wp-corrector', plugin_dir_url(__FILE__) . 'css/corrector.css');
+});
